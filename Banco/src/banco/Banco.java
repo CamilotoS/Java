9b8cdc57be21c24772c0f4dcf9005682524ae778
Camiloto_ <gameoverlock1@gmail.com>
2020-10-04 12:41:34 -0300

    Sistema de Senha de Banco
    Sistema para chamar determinada senha de banco com implementação de prioridade

---
+++ Banco/src/banco/Banco.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ives Camiloto
+ */
+public class Banco {
+    ArrayList<String> listaSenha = new ArrayList<>();   
+    String senhaTipo;
+    int numero;
+ 
+}
